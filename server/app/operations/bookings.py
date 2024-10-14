02445069146a3988409160da67fe05e1047ed48a
gomespgp <gabriel_p_g@hotmail.com>
2024-10-14 17:41:30 -0300

    feat: add booking.com bookings endpoint

--- server/app/operations/bookings.py
+++ server/app/operations/bookings.py
@@ -5,8 +5,11 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 
-from app.models.bookings import AirbnbBooking as AirbnbBookingDBModel
-from app.schemas.bookings import AirbnbBooking
+from app.models.bookings import (
+    AirbnbBooking as AirbnbBookingDBModel,
+    BookingcomBooking as BookingcomBookingDBModel
+)
+from app.schemas.bookings import AirbnbBooking, BookingcomBooking
 from app.schemas.http import Ok200, Created201
 from app.schemas.responses import Duplicated409
 
@@ -20,6 +23,23 @@ def get_airbnb_bookings(db_session: Session):
 
 def post_airbnb_bookings(db_session: Session, bookings: List[AirbnbBooking]):
     booking_models = [AirbnbBookingDBModel(**booking.model_dump()) for booking in bookings]
+    try:
+        db_session.add_all(booking_models)
+        db_session.commit()
+        return Created201(message=f"{len(bookings)} records created", records=bookings)
+    except IntegrityError as e:
+        return Duplicated409(detail=str(e))
+
+
+def get_bookingcom_bookings(db_session: Session):
+    bookings = jsonable_encoder((db_session.scalars(select(BookingcomBookingDBModel))).all())
+    if not bookings:
+        raise HTTPException(status_code=404, detail="No bookings found")
+    return Ok200(records=bookings)
+
+
+def post_bookingcom_bookings(db_session: Session, bookings: List[BookingcomBooking]):
+    booking_models = [BookingcomBookingDBModel(**booking.model_dump()) for booking in bookings]
     try:
         db_session.add_all(booking_models)
         db_session.commit()
